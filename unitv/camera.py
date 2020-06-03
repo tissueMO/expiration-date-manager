b29177c86296150b2085dd799662b60e933ae9c3
TomokiPC-Parent\tomokis <ts32worker@gmail.com>
2020-06-04 01:33:28 +0900

    [unitv] カメラ画像の送信を一定の間隔で行うように修正

--- unitv/camera.py
+++ unitv/camera.py
@@ -3,7 +3,7 @@
 ########################################################################
 from machine import UART
 from fpioa_manager import fm
-import sensor, lcd
+import sensor, lcd, time
 
 WIDTH = 160
 HEIGHT = 120
@@ -11,8 +11,8 @@ HEIGHT = 120
 lcd.init(freq=15000000)
 
 sensor.reset()
-sensor.set_pixformat(sensor.GRAYSCALE)
-# sensor.set_pixformat(sensor.RGB565)
+# sensor.set_pixformat(sensor.GRAYSCALE)
+sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.QVGA)
 sensor.set_windowing((WIDTH, HEIGHT))
 sensor.set_vflip(True)
@@ -24,7 +24,7 @@ sensor.skip_frames()
 fm.register(35, fm.fpioa.UART1_TX, force=True)
 fm.register(34, fm.fpioa.UART1_RX, force=True)
 
-uart = UART(UART.UART1, 921600, 8, 0, 0, timeout=1000, read_buf_len=4096)
+uart = UART(UART.UART1, 1152000, 8, 0, 0, timeout=1000, read_buf_len=4096)
 
 
 while True:
@@ -35,3 +35,5 @@ while True:
 
     # Grove ポートを通して M5Stack へ転送
     uart.write(img)
+
+    time.sleep(0.5)
