e826864481ef3e8799d3b7a23f7892b5d42f949a
TomokiPC-Parent\tomokis <ts32worker@gmail.com>
2020-06-06 00:36:55 +0900

    [server] Vision API の結果を使って賞味期限の年月日を抽出できるようになった

--- server/src/api/main.py
+++ server/src/api/main.py
@@ -5,20 +5,27 @@ import cv2
 import numpy as np
 import requests
 import json
+import re
 from typing import Any, Dict
 from configparser import ConfigParser
 
 # 独自モジュール読み込み
 from api import common
 
+# 定数定義
+# 賞味期限フォーマットに従って分解したときの格納順序と格納先のキー名を表したリスト
+EXPIRATION_DATE_KEYS = ["year" , "month", "day"]
+
 # 設定ファイル読み込み
 config = ConfigParser()
 config.read("settings.conf", encoding="utf-8")
 
 # 設定読み込み
+# 賞味期限フォーマットパターン
+DATE_FORMAT_PATTERNS = json.loads(config.get("format", "date_format_patterns"))
 # APIキー
 API_KEY = config.get("gcp", "api_key")
-VISION_API_URL = f"https://vision.googleapis.com/v1/images:annotate?key={API_KEY}";
+VISION_API_URL = f"https://vision.googleapis.com/v1/images:annotate?key={API_KEY}"
 
 def health(request) -> str:
     """ヘルスチェック用関数
@@ -83,15 +90,53 @@ def detect(request) -> Dict[str, Any]:
                     {
                         "type": "TEXT_DETECTION"
                     }
-                ]
+                ],
+                "imageContext": {
+                    "languageHints": ["en"]
+                }
             }
         ]
     }))
-    response_str = json.dumps(response.json(), ensure_ascii=False, indent=4)
+    response_json = response.json()
+    response_str = json.dumps(response_json, ensure_ascii=False, indent=4)
     print(response_str)
 
     # レスポンスをファイルに書き出し
     with open("./response.json", "w") as w:
-        w.write(json.dumps(response.json(), ensure_ascii=False, indent=4))
+        w.write(json.dumps(response_json, ensure_ascii=False, indent=4))
 
-    return "Detected OK."
+    # レスポンスのうち全文テキストを抜き出す
+    found_text = response_json \
+        .get("responses", [{}])[0] \
+        .get("textAnnotations", [{}])[0] \
+        .get("description")
+    print(f"found_test: {found_text}")
+
+    if found_text is None:
+        return {
+            "success": False,
+            "result": None
+        }
+
+    # 賞味期限っぽい部分を抜き出す
+    for date_format_pattern in DATE_FORMAT_PATTERNS:
+        # 正規表現で表されるパターンで抜き出す
+        print(date_format_pattern)
+        result = re.match(date_format_pattern, found_text)
+        if not result:
+            print("not found")
+            continue
+
+        # 年月日に分解し、日が省略されている場合は None として埋める
+        date_parts = list(result.groups())
+        expiration_date = {
+            key: date_parts[index] if index < len(date_parts) else None
+            for index, key in enumerate(EXPIRATION_DATE_KEYS)
+        }
+        print(expiration_date)
+        break
+
+    return {
+        "success": True,
+        "result": expiration_date
+    }
