efc62b8e3485f3fe9d1963fe34e99cbabca1cedc
TomokiPC-Parent\tomokis <ts32worker@gmail.com>
2020-06-09 02:13:16 +0900

    [unitv] メインループ方式変更、LED消灯、ボタン押下時の処理を追加

--- unitv/camera.py
+++ unitv/camera.py
@@ -1,12 +1,16 @@
 ########################################################################
 #   Groveポートを通して、カメラの画像をM5Stackにシリアル転送します。
 ########################################################################
+import sensor, lcd, time
 from machine import UART
 from fpioa_manager import fm
-import sensor, lcd, time
+from Maix import GPIO
+from board import board_info
+from modules import ws2812
 
 WIDTH = 320
 HEIGHT = 240
+INTERVAL_MS = 2000
 
 lcd.init(freq=15000000)
 
@@ -23,14 +27,29 @@ sensor.skip_frames()
 # UnitV の Grove ポートを通してシリアル通信する
 fm.register(35, fm.fpioa.UART1_TX, force=True)
 fm.register(34, fm.fpioa.UART1_RX, force=True)
-
 uart = UART(UART.UART1, 1152000, 8, 0, 0, timeout=2000, read_buf_len=4096)
 enabled = False
 
+# UnitV のボタンイベントを捕捉できるようにする
+fm.register(18, fm.fpioa.GPIO1)
+button_a=GPIO(GPIO.GPIO1, GPIO.IN, GPIO.PULL_DOWN)
+fm.register(19, fm.fpioa.GPIO2)
+button_b=GPIO(GPIO.GPIO2, GPIO.IN, GPIO.PULL_DOWN)
+
+# LED ランプを消す
+class_ws2812 = ws2812(8, 1)
+class_ws2812.set_led(0, (0, 0, 0))
+class_ws2812.display()
+
+previous_time_ms = time.ticks_ms()
 print("---- Camera Started ---------------------------------------")
 while True:
-    if enabled:
-        # Grove ポートを通して M5Stack へ転送
+    current_time_ms = time.ticks_ms()
+
+    if enabled and (current_time_ms - previous_time_ms) >= INTERVAL_MS:
+        # 一定間隔で Grove ポートを通して M5Stack へ転送
+        previous_time_ms = current_time_ms
+
         try:
             # 無圧縮ビットマップ画像を送信
             img = sensor.snapshot()
@@ -41,7 +60,8 @@ while True:
             lcd.display(img_shrinked)
 
             # JPEG圧縮 (サイズ可変)
-            img_buf = img.compress(quality=50)
+            # img_buf = img.compress(quality=50)
+            img_buf = img_shrinked.compress(quality=80)
 
             # 10 bytes の開始データを添えてJPEG画像を送信
             img_size1 = (img_buf.size() & 0xFF0000) >> 16
@@ -50,10 +70,9 @@ while True:
             data_packet = bytearray([0xFF, 0xD8, 0xEA, 0x01, img_size1, img_size2, img_size3, 0x00, 0x00, 0x00])
             uart.write(data_packet)
             uart.write(img_buf)
+            print("Sending Image to M5Stack Completed.")
         except:
-            pass
-
-        print("Sending Image Completed.")
+            print("Sending Image to M5Stack Failed.")
 
     # M5Stack からの命令を取得
     line =  uart.readline()
@@ -67,9 +86,15 @@ while True:
         statement = statement[:(len(statement) - 2)]
         if statement == "pause":
             enabled = False
-            print("Pausing...")
+            print("Pausing... (By M5Stack)")
         elif statement == "resume":
             enabled = True
-            print("Resumed.")
+            print("Resumed. (By M5Stack)")
 
-    time.sleep(2.0)
+    # ボタン入力を取得
+    if button_a.value() == 0 and enabled:
+        enabled = False
+        print("Pausing... (By A Button)")
+    if button_b.value() == 0 and not enabled:
+        enabled = True
+        print("Resumed. (By B Button)")
